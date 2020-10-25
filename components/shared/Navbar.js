4e8de6d351ada042a9f7b33a9052f791459adb16
Phillip Lang Martinez <philliplangmartinez@gmail.com>
2020-10-25 17:34:23 +0100

    graphql integration

--- components/shared/Navbar.js
+++ components/shared/Navbar.js
@@ -1,66 +1,49 @@
-import React from "react";
+import { Navbar, Nav } from "react-bootstrap";
+import Link from "next/link";
 
-const Navbar = () => {
+const AppLink = ({ children, className, href }) => {
+  return (
+    <Link href={href}>
+      <a className={className}>{children}</a>
+    </Link>
+  );
+};
+
+const AppNavbar = () => {
   return (
     <div className="navbar-wrapper">
-      <nav className="navbar navbar-expand-lg navbar-dark fj-mw9">
-        <a className="navbar-brand mr-3 font-weight-bold" href="#">
-          FilipJerga
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav">
-            <li className="nav-item mr-3">
-              <a className="nav-link" href="#">
-                Portfolio
-              </a>
-            </li>
-            <li className="nav-item mr-3">
-              <a className="nav-link" href="#">
-                Courses
-              </a>
-            </li>
-            <li className="nav-item mr-3">
-              <a className="nav-link" href="#">
-                Cv
-              </a>
-            </li>
-            <li className="nav-item mr-3">
-              <a className="nav-link" href="#">
-                Ask me
-              </a>
-            </li>
-          </ul>
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item mr-3">
-              <a className="nav-link" href="#">
-                Sign Up
-              </a>
-            </li>
-            <li className="nav-item mr-3">
-              <a
-                className="nav-link btn btn-success bg-green-2 bright"
-                href="#"
-              >
-                Sign In
-              </a>
-            </li>
-          </ul>
-        </div>
-      </nav>
+      <Navbar expand="lg" className="navbar-dark fj-mw9">
+        <AppLink className="navbar-brand mr-3 font-weight-bold" href="/">
+          Phillip Lang
+        </AppLink>
+        <Navbar.Toggle />
+        <Navbar.Collapse>
+          <Nav className="mr-auto">
+            <AppLink className="nav-link mr-3" href="/portfolios">
+              Portfolios
+            </AppLink>
+            <AppLink className="nav-link mr-3" href="/forum/categories">
+              Forum
+            </AppLink>
+            <AppLink className="nav-link mr-3" href="/cv">
+              Cv
+            </AppLink>
+          </Nav>
+          <Nav>
+            <AppLink className="nav-link mr-3" href="/login">
+              Sign In
+            </AppLink>
+            <AppLink
+              className="mr-3 btn btn-success bg-green-2 bright"
+              href="/register"
+            >
+              Sign Up
+            </AppLink>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     </div>
   );
 };
 
-export default Navbar;
+export default AppNavbar;
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,39 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/index.scss";
+import Navbar from "@/components/shared/Navbar";
+import Hero from "@/components/shared/Hero";
+import App from "next/app";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+const MyApp = ({ Component, pageProps }) => {
+  console.log(pageProps);
 
+  const isHomePage = () => Component.name === "Home";
+  return (
+    <div className="portfolio-app">
+      <Navbar />
+      {isHomePage() && <Hero />}
+      <div className="container">
+        <Component {...pageProps} />
+      </div>
+      {isHomePage() && (
+        <footer id="sticky-footer" className="py-4 bg-black text-white-50 py-3">
+          <div className="container text-center">
+            <small>Copyright &copy; Your Website</small>
+          </div>
+        </footer>
+      )}
+    </div>
+  );
+};
+
+MyApp.getInitialProps = async (context) => {
+  const initialProps =
+    App.getInitialProps && (await App.getInitialProps(context));
+  return {
+    pageProps: {
+      appData: "hello",
+      ...initialProps.pageProps,
+    },
+  };
+};
 export default MyApp;
